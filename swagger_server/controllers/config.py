1295e6eba44b0493715f2b2be6cec3a82fb14955
Sofia <sofia.chevrolat@madimedia.pro>
2020-12-14 07:22:45 -0400

    Attempting lowering size to fix heroku R14 error

--- swagger_server/controllers/config.py
+++ swagger_server/controllers/config.py
@@ -5,6 +5,40 @@ import io
 
 
 #---------------------- Functions ----------------------#
+def convert_types(df, print_info = False):
+    
+    original_memory = df.memory_usage().sum()
+    
+    # Iterate through each column
+    for c in df:
+        
+        # Convert ids and booleans to integers
+        if ('SK_ID' in c):
+            df[c] = df[c].fillna(0).astype(np.int32)
+            
+        # Convert objects to category
+        elif (df[c].dtype == 'object') and (df[c].nunique() < df.shape[0]):
+            df[c] = df[c].astype('category')
+        
+        # Booleans mapped to integers
+        elif list(df[c].unique()) == [1, 0]:
+            df[c] = df[c].astype(bool)
+        
+        # Float64 to float32
+        elif df[c].dtype == float:
+            df[c] = df[c].astype(np.float32)
+            
+        # Int64 to int32
+        elif df[c].dtype == int:
+            df[c] = df[c].astype(np.int32)
+        
+    new_memory = df.memory_usage().sum()
+    
+    if print_info:
+        print(f'Original Memory Usage: {round(original_memory / 1e9, 2)} gb.')
+        print(f'New Memory Usage: {round(new_memory / 1e9, 2)} gb.')
+        
+    return df
 
 def download_csv_to_pd(url):
     '''
@@ -15,7 +49,8 @@ def download_csv_to_pd(url):
     rawData = pd.read_csv(io.StringIO(urlData.decode('utf-8')), sep=",")
     rawData = rawData.drop(rawData.columns[0], axis=1)
 
-    return rawData
+    return convert_types(rawData)
+    
 #---------------------- Variables ----------------------#
 
 SAVED_DATA_BUCKET_URL = 'https://scoringapp.s3.eu-west-3.amazonaws.com/saved_data/'
