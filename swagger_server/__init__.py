b3bf4794647433b33c7064a751678b2a32ec0bf8
Sofia <sofia.chevrolat@madimedia.pro>
2020-12-14 08:56:59 -0400

    Putting Waitress server back

--- swagger_server/__init__.py
+++ swagger_server/__init__.py
@@ -8,9 +8,7 @@ def serve_api():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Credit Score Predictor'}, pythonic_params=True)
-    #serve(app, host="0.0.0.0", port=8080)
-    app.run(host='0.0.0.0', port=os.environ.get('PORT', '5000'))
-
+    serve(app, host="0.0.0.0", port=os.environ.get('PORT', '5000'))
     return app
 
 init_app = serve_api()
